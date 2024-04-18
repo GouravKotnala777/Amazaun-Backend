0edfd1269af1ff8e7177c278872782a2dfe254f1
gouravkotnala <gouravkotnala777@gmail.com>
2024-04-18 16:41:39 +0530

    -------

--- src/Amazaun/utils/utilitie-variables.ts
+++ src/Amazaun/utils/utilitie-variables.ts
@@ -1,6 +1,7 @@
 import { CookieOptions } from "express";
 
 export const options:CookieOptions = {
+    expires: new Date(Date.now() + 604800000), // 7 days
     sameSite:"none",
     secure:true,
     httpOnly:true
